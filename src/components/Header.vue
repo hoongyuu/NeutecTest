d9f78e0feb017ef41a1bbc58c4d2c535dd624e9a
Cheeto <kahod12450@gmail.com>
2023-09-02 12:15:51 +0800

    feat: 初始化佈局

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <h1>日新測驗</h1>
+
+    <!-- hamburger menu -->
+    <div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+  header {
+    background-color: #fff;
+    border-bottom: 1px solid #eaeaea;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 16px;
+  }
+</style>
