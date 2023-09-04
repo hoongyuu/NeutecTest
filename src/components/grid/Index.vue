175c06264aad9841895ac82f64e23af1753621f9
Cheeto <kahod12450@gmail.com>
2023-09-04 16:24:09 +0800

    feat: 方格增加閃爍

--- src/components/grid/Index.vue
+++ src/components/grid/Index.vue
@@ -4,7 +4,7 @@ import GridItem from './Item.vue'
 
 <template>
   <div class="grid">
-    <GridItem v-for="(num) in 9" :key="num" />
+    <GridItem v-for="(num) in 9" :key="num" :is-flash="[3, 5, 9].includes(num)">{{ num }}</GridItem>
   </div>
 </template>
 
--- src/components/grid/Item.vue
+++ src/components/grid/Item.vue
@@ -1,14 +1,40 @@
 <script setup lang="ts">
+const props = defineProps({
+  isFlash: {
+    type: Boolean,
+    default: false
+  }
+})
 </script>
 
 <template>
-  <div class="grid-item" />
+  <div :class="['grid-item', { 'isFlash': props.isFlash }]">
+    <slot />
+  </div>
 </template>
 
 <style scoped lang="scss">
+@keyframes flashing {
+  0% {
+    opacity: 1;
+  }
+
+  50% {
+    opacity: 0.6;
+  }
+}
+
 .grid-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 100px;
+  color: yellowgreen;
   border: black solid 2px;
   background: radial-gradient(circle, rgba(113, 81, 95, 1) 81%, rgba(0, 0, 0, 1) 100%);
+
+  &.isFlash {
+    animation: flashing 800ms linear infinite alternate;
+  }
 }
 </style>
