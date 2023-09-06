e93aa59cb3843a4ad4a78fd70668309fda039992
Cheeto <kahod12450@gmail.com>
2023-09-06 15:03:32 +0800

    feat: circle 增加各種方向的功能

--- src/components/grid/CircleAnimation.vue
+++ src/components/grid/CircleAnimation.vue
@@ -3,17 +3,93 @@
 import { onMounted, ref } from 'vue';
 import * as PIXI from "pixi.js";
 
-const circles = ref<PIXI.Graphics[]>([])
 const CIRCLE_RADIUS = 15
+const circles = ref<PIXI.Graphics[]>([])
+const app = ref<PIXI.Application | null>(null)
 
-const getItemElement = (itemNum: number): DOMRect => {
-  return document.querySelector(`#grid-item-${itemNum}`)!.getBoundingClientRect()
+const getItemElement = (itemNum: number): DOMRect =>
+  document.querySelector(`#grid-item-${itemNum}`)!.getBoundingClientRect()
+
+const directionMap = {
+  '13': 'right',
+  '46': 'right',
+  '79': 'right',
+  '31': 'left',
+  '64': 'left',
+  '97': 'left',
+  '17': 'down',
+  '28': 'down',
+  '39': 'down',
+  '71': 'up',
+  '82': 'up',
+  '93': 'up',
+  '37': 'left',
+  '19': 'right',
+  '73': 'right',
+  '91': 'left'
+}
+
+const drawCircle = ({ start, end }: {
+  start: number,
+  end: number
+}) => {
+  const circle = new PIXI.Graphics()
+  const { x: startX, y: startY, width: startW, height: startH } = getItemElement(start)
+  const { x: endX, y: endY, width: endW, height: endH } = getItemElement(end)
+  circle.startCenter = {
+    x: startX + startW / 2,
+    y: startY + startH / 2
+  }
+  circle.endCenter = {
+    x: endX + endW / 2,
+    y: endY + endH / 2
+  }
+  circle.beginFill(0xA5F12B);
+  circle.drawCircle(0, 0, CIRCLE_RADIUS);
+  circle.endFill()
+  circle.x = circle.startCenter.x
+  circle.y = circle.startCenter.y
+  circle.direction = directionMap[`${start}${end}`]
+
+  circle.customMove = function (delta) {
+    if (this.direction === 'up' && this.y <= 0 - CIRCLE_RADIUS) {
+      this.y = window.innerHeight + CIRCLE_RADIUS
+      return
+    }
+    if (this.direction === 'down' && this.y > window.innerHeight + CIRCLE_RADIUS) {
+      this.y = -CIRCLE_RADIUS
+      return
+    }
+    if (this.direction === 'left' && this.x <= 0 - CIRCLE_RADIUS) {
+      if (!this.boundaryH) {
+        this.boundaryH = Math.abs(this.y) - this.endCenter.y
+      }
+      this.x = window.innerWidth + CIRCLE_RADIUS
+      this.y = this.startCenter.y - this.boundaryH
+      return
+    }
+    if (this.direction === 'right' && this.x >= window.innerWidth + CIRCLE_RADIUS) {
+      if (!this.boundaryH) {
+        this.boundaryH = Math.abs(this.y) - this.endCenter.y
+      }
+      this.x = -CIRCLE_RADIUS
+      this.y = this.startCenter.y - this.boundaryH
+      return
+    }
+    const targetX = this.endCenter.x - this.startCenter.x
+    const targetY = this.endCenter.y - this.startCenter.y
+    const angle = Math.atan2(targetY, targetX)
+    const deltaX = 2 * delta * Math.cos(angle); // 計算X方向上的變化
+    const deltaY = 2 * delta * Math.sin(angle); // 計算Y方向上的變化
+    this.x += deltaX;
+    this.y += deltaY;
+  }
+  app.value.stage.addChild(circle)
+  circles.value.push(circle)
 }
 
 onMounted(() => {
-  console.log(window.devicePixelRatio);
-
-  const app = new PIXI.Application({
+  app.value = new PIXI.Application({
     width: window.innerWidth,
     height: window.innerHeight,
     backgroundAlpha: 0,
@@ -21,36 +97,16 @@ onMounted(() => {
     autoDensity: true
   })
 
-  const drawCircle = (num: number) => {
-    const circle = new PIXI.Graphics()
-    const { x, y, width, height } = getItemElement(num)
-    circle.beginFill(0xA5F12B);
-    circle.drawCircle(0, 0, CIRCLE_RADIUS);
-    circle.endFill()
-    circle.x = x + width / 2
-    circle.y = y + height / 2
-    circle.customMove = (delta) => {
-      if (circle.x > window.innerWidth + CIRCLE_RADIUS) {
-        circle.x = -CIRCLE_RADIUS
-        return
-      }
-      circle.x += 2 * delta
-    }
-    app.stage.addChild(circle)
-    circles.value.push(circle)
-  }
-
   const circleAnimation = document.getElementById('circle-animation')!
 
-  circleAnimation.appendChild(app.view)
+  circleAnimation.appendChild(app.value.view)
 
-  // 繪製圓形
-  drawCircle(1)
-  drawCircle(3)
-  drawCircle(7)
-  drawCircle(9)
+  drawCircle({
+    start: 1,
+    end: 9
+  })
 
-  app.ticker.add(function (delta) {
+  app.value.ticker.add(function (delta) {
     circles.value.forEach((circle) => {
       circle.customMove(delta)
     })
